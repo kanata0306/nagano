128882263dcb6192be354407b6a7147c9fd1343f
EC2 Default User <ec2-user@ip-172-31-13-44.ap-northeast-1.compute.internal>
2024-09-16 03:12:18 +0000

    [add]admin custmer route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
- 
-  devise_for :costomers
+  
+  devise_for :costomers, controllers: {
+      registrations: "public/registrations",
+      sessions: "public/sessions"
+  }
+  
+  devise_for :admin, controllers: {
+      registrations: "admin/sessions"
+  }
 end

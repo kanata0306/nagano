ae34aaf24563d318bf3dbb8c9f33b475875d18ee
EC2 Default User <ec2-user@ip-172-31-13-44.ap-northeast-1.compute.internal>
2024-09-21 07:20:04 +0000

    [add] admin migration

---
+++ db/migrate/20240921071609_create_admins.rb
@@ -0,0 +1,11 @@
+class CreateAdmins < ActiveRecord::Migration[6.1]
+  def change
+    create_table :admins do |t|
+      
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password,           null: false, default: ""
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_13_145001) do
+ActiveRecord::Schema.define(version: 2024_09_21_071609) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "costomers", force: :cascade do |t|
     t.string "email", default: "", null: false
